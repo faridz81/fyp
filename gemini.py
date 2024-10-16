9d3fd1d4e091049c075e6e6f3c321f10f8d56dd9
faridz81 <87579093+faridz81@users.noreply.github.com>
2024-10-17 07:35:24 +0800

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -28,23 +28,23 @@ def get_text_chunks(data):
     for entry in data:
         for key, value in entry.items():
             if key == "id_kehadiran":
-                description = f"id_kehadiran: {value}. This column represents the unique attendance ID for the record."
+                description = f"id_kehadiran: {value}. This key represents the unique attendance ID for the record."
             elif key == "name":
-                description = f"name: {value}. This column represents the full name of the student."
+                description = f"name: {value}. This key represents the full name of the student."
             elif key == "nama_subject":
-                description = f"nama_subject: {value}. This column represents the name of the classroom's subject."
+                description = f"nama_subject: {value}. This key represents of the class name. "
             elif key == "info_kelas":
-                description = f"info_kelas: {value}. This column represents the info of the classroom."
+                description = f"info_kelas: {value}. This key represents the class section."
             elif key == "no_matriks":
-                description = f"no_matriks: {value}. This column represents the student's matriculation number."
+                description = f"no_matriks: {value}. This key represents the student's matriculation number."
             elif key == "nama_fakulti":
-                description = f"nama_fakulti: {value}. This column represents the name of the faculty the student is enrolled in."
+                description = f"nama_fakulti: {value}. This key represents the name of the faculty the student is enrolled in."
             elif key == "nama_course":
-                description = f"nama_course: {value}. This column represents the name of the course the student is taking."
+                description = f"nama_course: {value}. This key represents the name of the course the student is taking."
             elif key == "register_time":
-                description = f"register_time: {value}. This column represents the date and time when the student punch their attendance."
+                description = f"register_time: {value}. This key represents the date and time when the student punched their attendance."
             elif key == "lokasi_device":
-                description = f"lokasi_device: {value}. This column represents the location of the device used to register the attendance. If device is phone, means attendance take by using QR code generate by lecturer's phone"
+                description = f"lokasi_device: {value}. This key represents the location of the device used to register the attendance."
             else:
                 description = f"{key}: {value}."
             
@@ -58,7 +58,6 @@ def get_text_chunks(data):
     
     return chunks
 
-
 # Get embeddings for each chunk
 def get_vector_store(chunks):
     embeddings = GoogleGenerativeAIEmbeddings(
@@ -70,12 +69,12 @@ def get_vector_store(chunks):
 def get_conversational_chain():
     prompt_template = """
     You are a JSON data assistant. Your task is to assist users in retrieving information from the provided JSON data.
-    Data is about attendance log of the student.
+    Data is about attendance log of the student of my class. My class means that I am a lecturer. 
     Student name with the most row of data log, got the highest attendance performance.
+    Same class name and section to be considered as a same one class.
+    Answer with natural language, don't use json code or other code as answer. Explain your answer. Be friendly.
     Your goal is to provide accurate answers based on the context of the data, including partial name matching and identifying students with high attendance.
 
-
-    
     Context:\n {context}?\n
     Question: \n{question}\n
 
@@ -85,8 +84,7 @@ def get_conversational_chain():
     model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
                                    client=genai,
                                    temperature=0.1,
-                                   top_k=10,
-                                   )
+                                   top_k=10)
     prompt = PromptTemplate(template=prompt_template,
                             input_variables=["context", "question"])
     chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
@@ -94,11 +92,11 @@ def get_conversational_chain():
 
 def clear_chat_history():
     st.session_state.messages = [
-        {"role": "assistant", "content": "Upload a JSON URL and ask me a question"}]
+        {"role": "assistant", "content": "Ask me a question about the attendance data."}]
 
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001")  # type: ignore
+        model="models/embedding-001")
 
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True) 
     docs = new_db.similarity_search(user_question, top_k=10)
@@ -112,39 +110,34 @@ def user_input(user_question):
 
 def main():
     st.set_page_config(
-        page_title="Gemini JSON Chatbot",
+        page_title="Smart Attendance AI Assistant",
         page_icon="🤖"
     )
 
-    # Sidebar for entering JSON URL
-    with st.sidebar:
-        st.title("Menu:")
-        json_url = st.text_input("Enter the URL of your JSON data")
+    # Get the lecturer ID from the GET parameter
+    query_params = st.query_params
+    lecturer_id = query_params.get("id", None)
+
+    if lecturer_id:
+        json_url = f"https://fyp.smartsolah.com/kehadiranApi/kehadiranMyStudent/{lecturer_id}"
         
-        if st.button("Submit & Process"):
-            if json_url:
-                with st.spinner("Processing..."):
-                    raw_data = read_json_data(json_url)
-                    text_chunks = get_text_chunks(raw_data)
-                    if text_chunks:  # Check if text_chunks is not empty
-                        get_vector_store(text_chunks)
-                        st.success("Done")
-                    else:
-                        st.warning("No data found in the JSON.")
+        with st.spinner("Processing..."):
+            raw_data = read_json_data(json_url)
+            text_chunks = get_text_chunks(raw_data)
+            if text_chunks:
+                get_vector_store(text_chunks)
+                st.success("Data pelajar telah diproses")
             else:
-                st.warning("Please enter a JSON URL.")
-                return  # Return to avoid further execution
+                st.warning("Tiada data")
+    else:
+        st.error("Sila berikan maklumat pensyarah")
 
-    # Main content area for displaying chat messages
-    st.title("Chat with JSON data using Gemini🤖")
+    st.title("Smart Attendance AI Assistant")
     st.write("Welcome to the chat!")
-    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-    # Chat input
-    # Placeholder for chat messages
     if "messages" not in st.session_state.keys():
         st.session_state.messages = [
-            {"role": "assistant", "content": "Upload a JSON URL and ask me a question"}]
+            {"role": "assistant", "content": "Sila bertanya berkenaan maklumat kehadiran sahaja."}]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -158,7 +151,7 @@ def main():
     # Display chat messages and bot response
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
+            with st.spinner("Sedang Proses..."):
                 response = user_input(prompt)
                 placeholder = st.empty()
                 full_response = ''
