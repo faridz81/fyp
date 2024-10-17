222965f64c031f20c59492e20d620d8b43c10e75
faridz81 <87579093+faridz81@users.noreply.github.com>
2024-10-17 15:47:29 +0800

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -59,7 +59,7 @@ def get_text_chunks(data):
 # Get embeddings for each chunk
 def get_vector_store(chunks):
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="text-embedding-001"
+        model="models/embedding-001"
     )
     vector_store = FAISS.from_texts(chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
