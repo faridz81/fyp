c913ad7f55f410f5a53234eaf687f4b5b313febc
faridz81 <87579093+faridz81@users.noreply.github.com>
2024-10-17 15:54:23 +0800

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -66,14 +66,7 @@ def get_vector_store(chunks):
 
 def get_conversational_chain():
     prompt_template = """
-    You are a data assistant. Your task is to assist users in retrieving information from the provided JSON data.
-    Data is about attendance log of the student of my class. My class means that I am a lecturer. Each one json object contains one attendance of a student.
-    Count number of object with same student name, to count total attendance for each student.
-    IMPORTANT! You determine a class is distinct based on class_name. Do not use partial match for class name.
-    Use date and time in easy readable format.
-    Different punched date and time is consider different attendance, although student attend the same class name.
-    Answer with natural language, don't use json code or other code as answer. Answer in Malay if question in Malay. Answer in English if Question in English. Express count number by digit not text.  Explain your answer. Be friendly.
-    DO not always use table. If data more than two column use table to show data.
+
 
     Context:\n {context}?\n
     Question: \n{question}\n
@@ -83,7 +76,7 @@ def get_conversational_chain():
 
     model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
                                    client=genai,
-                                   temperature=0.9,
+                                   temperature=0.1,
                                    top_k=10)
     prompt = PromptTemplate(template=prompt_template,
                             input_variables=["context", "question"])
