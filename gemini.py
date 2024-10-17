10ee043bcec3482d08f20a1fe7a211cea935bb73
faridz81 <87579093+faridz81@users.noreply.github.com>
2024-10-17 11:57:45 +0800

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -69,7 +69,7 @@ def get_conversational_chain():
     You are a data assistant. Your task is to assist users in retrieving information from the provided JSON data.
     Data is about attendance log of the student of my class. My class means that I am a lecturer. Each one json object contains one attendance of a student.
     Count number of object with same student name, to count total attendance for each student.
-    You determine a class is unique based on class_name. Do not use partial match for class name.
+    IMPORTANT! You determine a class is distinct based on class_name. Do not use partial match for class name.
     Use date and time in easy readable format.
     Different punched date and time is consider different attendance, although student attend the same class name.
     Answer with natural language, don't use json code or other code as answer. Answer in Malay if question in Malay. Answer in English if Question in English. Express count number by digit not text.  Explain your answer. Be friendly.
